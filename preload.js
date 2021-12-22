67f02da744664892ba800784d2ae1c89ac4e5678
moeyua <moeyua@foxmail.com>
2021-12-22 15:54:10 +0800

    none UI moede

---
+++ preload.js
@@ -0,0 +1,15 @@
+const utools = window.utools;
+
+window.exports = {
+    "YouGlish": {
+       mode: "none",
+       args: {
+          enter: (action) => {
+             // action = { code, type, payload }
+             utools.hideMainWindow()
+             utools.ubrowser.goto('https://youglish.com/pronounce/' + action.payload + '/english?').run()
+             utools.outPlugin()
+          }  
+       } 
+    }
+ }
